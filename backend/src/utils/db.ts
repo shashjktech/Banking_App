0e89bf171a4026ad6b62e30819894c323806c071
Shashwat Pandey <shashwat.pandey@jktech.com>
2026-09-06 19:05:48 +0530

    updated pg pool

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -4,10 +4,16 @@ import { env } from '../config/env';
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    connectionString: env.databaseUrl,
+    // connectionString: env.databaseUrl,
+    // max: 10,
+    // idleTimeoutMillis: 30000,
+    //ssl: { rejectUnauthorized: false }
+    user: 'postgres',
+    password: 'admin123', 
+    database: 'internal_banking_app',
+    host: '/cloudsql/project-242bd360-d8b9-4aad-9e6:asia-south1:internal-banking-app',
     max: 10,
     idleTimeoutMillis: 30000,
-    //ssl: { rejectUnauthorized: false }
 })
 pool.on('connect', () => {
   console.log('✅ PostgreSQL client connected successfully');
